78475cc059e6b2400ef459f4f528101671c785b8
anish333 <anishreddydakareddygari@gmail.com>
2026-01-07 14:42:08 +0530

    Made few changes

--- app_logistic.py
+++ app_logistic.py
@@ -140,16 +140,16 @@ if st.button("Predict Churn"):
 
     if prediction == 1:
         st.markdown(
-            f"<div class='result-box danger'>"
-            f"<h4>Customer is likely to Discontinue with the company</h4>"
+            f"<div class='result-box safe'>"
+            f"<h4>Customer is likely to continue with the company</h4>"
             f"<p>Probability: {probability:.2f}</p>"
             f"</div>",
             unsafe_allow_html=True
         )
     else:
         st.markdown(
-            f"<div class='result-box safe'>"
-            f"<h4>Customer is  likely to continue with the company</h4>"
+            f"<div class='result-box danger'>"
+            f"<h4>Customer is  likely to discontinue with the company</h4>"
             f"<p>Probability: {probability:.2f}</p>"
             f"</div>",
             unsafe_allow_html=True
